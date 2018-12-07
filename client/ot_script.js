5f6505b9441032ab419f27b472fdcd5c550cd42d
Fdebijl <18357629+Fdebijl@users.noreply.github.com>
2018-12-08 00:39:39 +0100

    Initial code commit

---
+++ client/ot_script.js
@@ -0,0 +1,222 @@
+(() => {
+  'use strict';
+
+  // Media definitions
+  const MEDIA = [
+    {
+      name: 'NOS',
+      id_mask: '[0-9]{7}',
+      match_domain: ['nos.nl', 'jeugdjournaal.nl'],
+      title_query: '.article__title',
+    },
+    {
+      name: 'AD',
+      id_mask: '~([a-z0-9]){6,8}',
+      match_domain: [
+        'ad.nl',
+        'bd.nl',
+        'ed.nl',
+        'tubantia.nl',
+        'bndestem.nl',
+        'pzc.nl',
+        'destentor.nl',
+        'gelderlander.nl',
+      ],
+      title_query: 'header.article__header > .article__title',
+    },
+    {
+      name: 'NU.nl',
+      id_mask: '([0-9]){7}',
+      match_domain: ['nu.nl'],
+      title_query: '.headerimage > .title.fluid',
+    },
+    {
+      name: 'Telegraaf',
+      id_mask: '([0-9]){7}',
+      match_domain: ['telegraaf.nl'],
+      title_query: 'h1.article-title',
+    },
+    {
+      name: 'Tweakers',
+      id_mask: '([0-9]){4,6}',
+      match_domain: ['tweakers.net'],
+      title_query: '.headingContent > .title > h1',
+    },
+    {
+      name: 'NRC',
+      id_mask: '([0-9]){7}',
+      match_domain: ['nrc.nl'],
+      title_query: 'div.center-block.intro-col.article__header > h1',
+    },
+    {
+      name: 'Volkskrant',
+      id_mask: '~([a-z0-9]){8}',
+      match_domain: ['volkskrant.nl'],
+      title_query: '.artstyle__header-title',
+    },
+    {
+      name: 'RTL',
+      id_mask: '([0-9]){7}',
+      match_domain: ['rtlnieuws.nl'],
+      title_query: 'div.article-title-width > h1.node-title',
+    },
+    {
+      name: 'Trouw',
+      id_mask: '~([a-z0-9]){8}',
+      match_domain: ['trouw.nl'],
+      title_query: 'h1.article__header__title',
+    },
+    {
+      name: 'Parool',
+      id_mask: '([0-9]){7}',
+      match_domain: ['parool.nl'],
+      title_query: 'h1.article__title',
+    },
+    {
+      name: 'Limburger',
+      id_mask: '([a-z]{3}[0-9]{8}_[0-9]{8})',
+      match_domain: ['limburger.nl'],
+      title_query: 'div > header.article__header > h1',
+    },
+    {
+      name: 'FD',
+      id_mask: '([0-9]){7}',
+      match_domain: ['fd.nl'],
+      title_query: '.head.full.social-quotable > h1',
+    },
+    {
+      name: 'DVHN',
+      id_mask: '([0-9]){8}',
+      match_domain: ['dvhn.nl'],
+      title_query: 'article.artikeltekst > header > h1',
+    },
+    {
+      name: 'HVNL',
+      id_mask: '([0-9]){7}',
+      match_domain: ['hartvannederland.nl'],
+      title_query: 'div.header-data > h1.article-heading',
+    },
+    {
+      name: 'LC',
+      id_mask: '([0-9]){8}',
+      match_domain: ['lc.nl'],
+      title_query: 'article > header > h1',
+    },
+    {
+      name: 'RD',
+      id_mask: '([0-9]{1}\\.[0-9]{7})',
+      match_domain: ['rd.nl'],
+      title_query: '#story-heading',
+    },
+    {
+      name: 'Elsevier',
+      id_mask: '([0-9]){6}',
+      match_domain: ['elsevierweekblad.nl'],
+      title_query: 'main > article > header > h1.entry-title',
+    },
+    {
+      name: 'BNR',
+      id_mask: '([0-9]){8}',
+      match_domain: ['bnr.nl'],
+      title_query: 'article.full > h1',
+    },
+  ];
+
+  // Find out which medium's website we're on
+  const medium = MEDIA.find((entry) => {
+    return entry.match_domain.includes(
+        window.location.hostname.replace('www.', '')
+    );
+  });
+
+  if (!medium) {
+    console.warn(
+        'OpenTitles script was executed, but the current domain is not present in the media list.',
+        'Either the media list needs to be ammended or the manifest contains a superflous URL.'
+    );
+    return;
+  }
+
+  if (!document.querySelector(medium.title_query)) {
+    console.warn(
+        'OpenTitles script was executed, but the current page doesn\'t contain a title element.'
+    );
+    return;
+  }
+
+  /**
+   * Build the modal and button and inject these into the DOM.
+   * @param {object} data The response object from the OpenTitles API.
+   */
+  function buildModal(data) {
+    document.body.classList.add(medium.name.replace(/\./gi, ''));
+
+    // Append 'clock' symbol to title
+    const titleElement = document.querySelector(medium.title_query);
+    const inner = titleElement.innerHTML;
+    const append = document.createElement('div');
+    append.title = 'Click to open the OpenTitles overlay';
+    append.innerHTML = '';
+    append.classList.add('opentitles__button', 'opentitles__histicon');
+
+    // Append information card to symbol
+    const card = document.createElement('div');
+    card.classList.add('opentitles__container');
+    card.innerHTML = '<div class="opentitles__titlemeta"></div><ul class="opentitles__titlelist"></ul>';
+    document.body.appendChild(card);
+    titleElement.innerHTML = inner + append.outerHTML;
+
+    // Article meta
+    document.querySelector('.opentitles__titlemeta').innerHTML = `<span><i class="opentitles__histicon" aria-hidden="true"></i> OpenTitles</span><div class="opentitles__closemodal">×</div>`;
+    document.querySelector('.opentitles__button').onclick = toggleModal;
+    document.querySelector('.opentitles__closemodal').onclick = toggleModal;
+
+    data.titles.forEach((title) => {
+      document.querySelector('.opentitles__titlelist').innerHTML += `<li class="opentitles__titleitem"><span class="opentitles__titledate">${title.datetime}: </span>${title.title}</li>`;
+    });
+  }
+
+  const toggleModal = () => {
+    document
+        .querySelector('.opentitles__container')
+        .classList.toggle('opentitles__verstoppertje');
+  };
+
+  /**
+   * Make the request to the OpenTitles API
+   */
+  function sendRekest() {
+    const request = new XMLHttpRequest();
+    const id = window.location.href.match(medium.id_mask)[0];
+
+    request.open(
+        'GET',
+        `https://floris.amsterdam/opentitles/article/${medium.name}/${id}`,
+        true
+    );
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        const data = JSON.parse(request.responseText);
+
+        if (data) {
+          // No use in poluting the DOM when we don't have any data anyway
+          console.info('Received response from API');
+          buildModal(data);
+        } else {
+          console.info('No content from OpenTitles API');
+        }
+      } else {
+        console.error(`OpenTitles API could not be reached: ${request.status}: ${request.statusText}`);
+      }
+    };
+
+    request.onerror = function() {
+      console.error(`OpenTitles API could not be reached: ${request.status}: ${request.statusText}`);
+    };
+
+    request.send();
+  }
+
+  sendRekest();
+})();
---
+++ server/server.js
@@ -0,0 +1,190 @@
+(() => {
+  'use strict';
+
+  const Parser = require('rss-parser');
+  const parser = new Parser({
+    headers: {'User-Agent': 'OpenTitles Scraper by floris@debijl.xyz'},
+  });
+  const moment = require('moment');
+  const fs = require('fs');
+  const express = require('express');
+  const app = express();
+  const cors = require('cors');
+  const MongoClient = require('mongodb').MongoClient;
+  const url = 'mongodb://localhost:27017/opentitles';
+  const CONFIG = JSON.parse(fs.readFileSync('config.json'));
+
+  let dbo;
+
+  /**
+   * Connect once and use the dbo reference in every call from here on out
+   */
+  MongoClient.connect(url, {
+    appname: 'OpenTitles API',
+    useNewUrlParser: true,
+    connectTimeoutMS: 5000,
+  }, function(err, database) {
+    if (err) {
+      throw err;
+    }
+
+    dbo = database.db('opentitles');
+  });
+
+  setInterval(() => {
+    retrieveArticles();
+  }, CONFIG.SCRAPER_INTERVAL * 1000);
+
+  /**
+   * Iterate over all RSS feeds and check for each article if we've seen it already or not
+   */
+  async function retrieveArticles() {
+    for (let i = 0; i < CONFIG.FEEDS.length; i++) {
+      const SUBFEED = CONFIG.FEEDS[i];
+      for (let j = 0; j < SUBFEED.FEEDS.length; j++) {
+        const feedname = SUBFEED.FEEDS[j];
+        let feed = await parser.parseURL(SUBFEED.PREFIX + feedname + SUBFEED.SUFFIX);
+
+        feed.items = feed.items.map((item) => {
+          item.artid = guidReducer(item[SUBFEED.ID_CONTAINER], SUBFEED.ID_MASK);
+          item.org = SUBFEED.NAME;
+          return item;
+        });
+
+        removeDupesAndCheck(feed);
+      }
+    }
+  }
+
+  /**
+   * Takes a fully populated feed from one org, removes the duplicates and passes it on to the database to check for new titles.
+   * @param {object} feed The feed as retrieved by rss-parser - make sure artid and org are populated.
+   */
+  function removeDupesAndCheck(feed) {
+    // Reduce feed items to unique ID's only
+    let seen = {};
+    feed.items = feed.items.filter((item) => {
+      // Make sure we have an articleID and organisation
+      if (!item.artid || !item.org) {
+        return false;
+      }
+
+      return seen.hasOwnProperty(item.artid) ? false : (seen[item.artid] = true);
+    });
+
+    feed.items.forEach((item) => {
+      checkWithDB(item);
+    });
+  }
+
+  /**
+   * Check with the database if the given article exists and update the title if we have a new one.
+   * @param {object} article An item as specified in the RSS standard, containing atleast the articleid and org.
+   */
+  function checkWithDB(article) {
+    const find = {
+      org: article.org,
+      articleID: article.artid,
+    };
+
+    findArticle(find, (res) => {
+      if (res === [null]) {
+        return;
+      }
+
+      if (!res) {
+        // Does not exit yet in DB
+        let newEntry = {
+          org: article.org,
+          articleID: article.artid,
+          link: article.link,
+          titles: [{title: article.title, datetime: moment(article.pubDate).format('MMMM Do YYYY, h:mm:ss a')}],
+          first_seen: moment().format('MMMM Do YYYY, h:mm:ss a'),
+          pub_date: moment(article.pubDate).format('MMMM Do YYYY, h:mm:ss a'),
+        };
+
+        // console.log(`[${article.org}:${article.artid}] Added new article to collection`);
+
+        dbo.collection('articles').insertOne(newEntry);
+        return;
+      }
+
+      if (res && res.titles[res.titles.length - 1].title !== article.title) {
+        // Article was already seen but we have a new title, add the latest title
+        res.titles.push({title: article.title, datetime: moment().format('MMMM Do YYYY, h:mm:ss a')});
+        dbo.collection('articles').replaceOne(find, res);
+        // console.log(`[${article.org}:${article.artid}] New title added for article`);
+        return;
+      }
+
+      // Article was seen, but the title hasn't changed
+      // console.log(`[${article.org}:${article.artid}] Title unchanged`);
+    });
+  }
+
+  /**
+   * Find an article in the database for a given organisation and ID.
+   * @param {object} find Object with org and articleid to query with the DB.
+   * @param {function} callback Called once a result is found
+   * @return {object} Returns the article if found, null if not found and [null] if an error occured.
+   */
+  function findArticle(find, callback) {
+    dbo.collection('articles').findOne(find, function(err, res) {
+      if (err) {
+        console.log(err);
+        if (typeof(callback) == 'function') {
+          callback([null]);
+        }
+        return [null];
+      }
+
+      if (typeof(callback) == 'function') {
+        callback(res);
+      }
+      return res;
+    });
+  }
+
+  /**
+   * Reduce a given GUID (effectively a URL) to a full ID used for tracking the article.
+   * @param {string} guid The GUID for this article.
+   * @param {string} mask The regex mask to extract the ID with.
+   * @return {string} The article ID contained within the GUID.
+   */
+  function guidReducer(guid, mask) {
+    const matches = guid.match(mask);
+    if (!matches) {
+      return false;
+    } else {
+      return matches[0];
+    }
+  }
+
+  // API Endpoints
+  app.get('/opentitles/article/:org/:id', cors({credentials: true, origin: true}), function(req, res) {
+    const artid = req.params.id;
+    const artorg = req.params.org;
+
+    if (!artid || !artorg || !artid.match(/[a-z0-9]+/)) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const find = {
+      org: artorg,
+      articleID: artid,
+    };
+
+    dbo.collection('articles').findOne(find, function(err, article) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(article, null, 4));
+    });
+  });
+
+  app.listen(8083);
+})();
