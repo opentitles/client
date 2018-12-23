e8afc51e5a8f68157af43cc9e356d7cba62d4a54
Fdebijl <18357629+Fdebijl@users.noreply.github.com>
2018-12-24 00:04:28 +0100

    Use unified media file

--- client/ot_script.js
+++ client/ot_script.js
@@ -1,158 +1,106 @@
 (() => {
   'use strict';
 
-  // Media definitions
-  const MEDIA = [
-    {
-      name: 'NOS',
-      id_mask: '[0-9]{7}',
-      match_domain: ['nos.nl', 'jeugdjournaal.nl'],
-      title_query: '.article__title',
-    },
-    {
-      name: 'AD',
-      id_mask: '~([a-z0-9]){6,8}',
-      match_domain: [
-        'ad.nl',
-        'bd.nl',
-        'ed.nl',
-        'tubantia.nl',
-        'bndestem.nl',
-        'pzc.nl',
-        'destentor.nl',
-        'gelderlander.nl',
-      ],
-      title_query: 'header.article__header > .article__title',
-    },
-    {
-      name: 'NU.nl',
-      id_mask: '([0-9]){7}',
-      match_domain: ['nu.nl'],
-      title_query: '.headerimage > .title.fluid',
-    },
-    {
-      name: 'Telegraaf',
-      id_mask: '([0-9]){7}',
-      match_domain: ['telegraaf.nl'],
-      title_query: 'h1.article-title',
-    },
-    {
-      name: 'Tweakers',
-      id_mask: '([0-9]){4,6}',
-      match_domain: ['tweakers.net'],
-      title_query: '.headingContent > .title > h1',
-    },
-    {
-      name: 'NRC',
-      id_mask: '([0-9]){7}',
-      match_domain: ['nrc.nl'],
-      title_query: 'div.center-block.intro-col.article__header > h1',
-    },
-    {
-      name: 'Volkskrant',
-      id_mask: '~([a-z0-9]){8}',
-      match_domain: ['volkskrant.nl'],
-      title_query: '.artstyle__header-title',
-    },
-    {
-      name: 'RTL',
-      id_mask: '([0-9]){7}',
-      match_domain: ['rtlnieuws.nl'],
-      title_query: 'div.article-title-width > h1.node-title',
-    },
-    {
-      name: 'Trouw',
-      id_mask: '~([a-z0-9]){8}',
-      match_domain: ['trouw.nl'],
-      title_query: 'h1.article__header__title',
-    },
-    {
-      name: 'Parool',
-      id_mask: '([0-9]){7}',
-      match_domain: ['parool.nl'],
-      title_query: 'h1.article__title',
-    },
-    {
-      name: 'Limburger',
-      id_mask: '([a-z]{3}[0-9]{8}_[0-9]{8})',
-      match_domain: ['limburger.nl'],
-      title_query: 'div > header.article__header > h1',
-    },
-    {
-      name: 'FD',
-      id_mask: '([0-9]){7}',
-      match_domain: ['fd.nl'],
-      title_query: '.head.full.social-quotable > h1',
-    },
-    {
-      name: 'DVHN',
-      id_mask: '([0-9]){8}',
-      match_domain: ['dvhn.nl'],
-      title_query: 'article.artikeltekst > header > h1',
-    },
-    {
-      name: 'HVNL',
-      id_mask: '([0-9]){7}',
-      match_domain: ['hartvannederland.nl'],
-      title_query: 'div.header-data > h1.article-heading',
-    },
-    {
-      name: 'LC',
-      id_mask: '([0-9]){8}',
-      match_domain: ['lc.nl'],
-      title_query: 'article > header > h1',
-    },
-    {
-      name: 'RD',
-      id_mask: '([0-9]{1}\\.[0-9]{7})',
-      match_domain: ['rd.nl'],
-      title_query: '#story-heading',
-    },
-    {
-      name: 'Elsevier',
-      id_mask: '([0-9]){6}',
-      match_domain: ['elsevierweekblad.nl'],
-      title_query: 'main > article > header > h1.entry-title',
-    },
-    {
-      name: 'BNR',
-      id_mask: '([0-9]){8}',
-      match_domain: ['bnr.nl'],
-      title_query: 'article.full > h1',
-    },
-  ];
+  const extapi = getBrowserAPI();
+  makeRequest(extapi.extension.getURL('/media.json')).then((result) => {
+    if (typeof(result) !== 'object') {
+      result = JSON.parse(result);
+    }
 
-  // Find out which medium's website we're on
-  const medium = MEDIA.find((entry) => {
-    return entry.match_domain.includes(
-        window.location.hostname.replace('www.', '')
-    );
+    let medium;
+
+    for (const key in result.FEEDS) {
+      if (result.FEEDS.hasOwnProperty(key)) {
+        const feed = result.FEEDS[key];
+
+        medium = feed.find((entry) => {
+          return entry.MATCH_DOMAINS.includes(window.location.hostname.replace('www.', ''));
+        });
+
+        if (medium) {
+          break;
+        }
+      }
+    }
+
+    // No entry for medium, exit.
+    if (!medium) {
+      return;
+    }
+
+    // No title element present - should definitely not happen in prod but it's here anyway for "graceful" degradation.
+    if (!document.querySelector(medium.TITLE_QUERY)) {
+      console.warn(
+          'OpenTitles script was executed, but the current page doesn\'t contain a title element.'
+      );
+      return;
+    }
+
+    makeRequest(`https://floris.amsterdam/opentitles/article/${medium.NAME}/${window.location.href.match(medium.ID_MASK)[0]}`).then((titlehist) => {
+      if (typeof(titlehist) !== 'object') {
+        titlehist = JSON.parse(titlehist);
+      }
+
+      buildModal(titlehist, medium);
+    });
   });
 
-  if (!medium) {
-    console.warn(
-        'OpenTitles script was executed, but the current domain is not present in the media list.',
-        'Either the media list needs to be ammended or the manifest contains a superflous URL.'
-    );
-    return;
+  /**
+   * Make a request to a given URL - use with 'await'.
+   * @param {String} url The target for the XMLHttpRequest.
+   * @param {String} method The HTTP method to use - defaults to 'GET'.
+   * @return {Promise} A promise that resolves with the result of the XMLHttpRequest to the given URL.
+   */
+  function makeRequest(url, method = 'GET') {
+    return new Promise((resolve, reject) => {
+      const xhr = getXHR();
+      xhr.open(method, url);
+      xhr.onload = function() {
+        if (this.status >= 200 && this.status < 300) {
+          resolve(xhr.response);
+        } else {
+          reject({
+            status: this.status,
+            statusText: xhr.statusText,
+          });
+        }
+      };
+      xhr.onerror = function() {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+        });
+      };
+      xhr.send();
+    });
   }
 
-  if (!document.querySelector(medium.title_query)) {
-    console.warn(
-        'OpenTitles script was executed, but the current page doesn\'t contain a title element.'
-    );
-    return;
+  /**
+   * Get the browser API object regardless of browser.
+   * @return {Object} Browser extension API.
+   */
+  function getBrowserAPI() {
+    try {
+      return browser;
+    } catch (e) {
+      try {
+        return chrome;
+      } catch (e) {
+        return null;
+      }
+    }
   }
 
   /**
    * Build the modal and button and inject these into the DOM.
-   * @param {object} data The response object from the OpenTitles API.
+   * @param {Object} data The response object from the OpenTitles API.
+   * @param {Object} medium The medium corresponding to this domain, as defined in media.json.
    */
-  function buildModal(data) {
-    document.body.classList.add(medium.name.replace(/\./gi, ''));
+  function buildModal(data, medium) {
+    document.body.classList.add(medium.NAME.replace(/\./gi, ''));
 
     // Append 'clock' symbol to title
-    const titleElement = document.querySelector(medium.title_query);
+    const titleElement = document.querySelector(medium.TITLE_QUERY);
     const append = document.createElement('div');
     append.title = 'Click to open the OpenTitles overlay';
     append.textContent = '';
@@ -190,57 +138,23 @@
     });
   }
 
-  const toggleModal = () => {
-    document
-        .querySelector('.opentitles__container')
-        .classList.toggle('opentitles__verstoppertje');
+  /**
+   * Toggle the modal that contains the list of titles.
+   */
+  function toggleModal() {
+    document.querySelector('.opentitles__container').classList.toggle('opentitles__verstoppertje');
   };
 
   /**
-   * Compatiblity shim to retrieve the wrapped XHR object when using Firefox.
-   * @return {XMLHttpRequest} The propertly wrapped XHR object.
+   * Compatiblity shim to retrieve the wrapped XHR object - Firefox doesn't allow usage of the plain XMLHttpRequest.
+   * @return {XMLHttpRequest} The properly wrapped XHR object.
    */
-  function getXMLHttp() {
+  function getXHR() {
     try {
+      // eslint-disable-next-line new-cap
       return XPCNativeWrapper(new window.wrappedJSObject.XMLHttpRequest());
     } catch (evt) {
       return new XMLHttpRequest();
     }
   }
-
-  /**
-   * Make the request to the OpenTitles API
-   */
-  function sendRekest() {
-    const request = getXMLHttp();
-    const id = window.location.href.match(medium.id_mask)[0];
-
-    request.open(
-        'GET',
-        `https://floris.amsterdam/opentitles/article/${medium.name}/${id}`,
-        true
-    );
-
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        const data = JSON.parse(request.responseText);
-
-        if (data) {
-          // No use in poluting the DOM when we don't have any data anyway
-          buildModal(data);
-        } else {
-        }
-      } else {
-        console.error(`OpenTitles API could not be reached: ${request.status}: ${request.statusText}`);
-      }
-    };
-
-    request.onerror = function() {
-      console.error(`OpenTitles API could not be reached: ${request.status}: ${request.statusText}`);
-    };
-
-    request.send();
-  }
-
-  sendRekest();
 })();
