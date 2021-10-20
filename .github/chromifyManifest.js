ffffbc6d4e28d21f7f6084311e51a1c5e9c9e1a6
Floris de Bijl <floris@feedbackfruits.com>
2021-10-20 17:26:09 +0200

    refactor: upgrade to parcel v2 (#109)
    * refactor: upgrade parcel to v2
    
    * fix: popup not detecting media in media.json
    
    * test: update reporter to use pretty printing

--- .github/chromifyManifest.js
+++ .github/chromifyManifest.js
@@ -7,6 +7,9 @@ const path = require('path');
 
 const pathToManifest = path.resolve(`dist-chrome/manifest.json`);
 
+console.log('=====================================')
+console.log('Running manifest chromifier')
+
 fs.access(pathToManifest, (err) => {
   if (err) {
     console.log('Manifest does not exist in Chrome build folder');
@@ -15,6 +18,7 @@ fs.access(pathToManifest, (err) => {
     manifest = JSON.parse(manifest);
     delete manifest.applications;
     fs.writeFileSync(pathToManifest, JSON.stringify(manifest, null, 4));
-    console.log('Removed applications key from manifest')
+    console.log('Removed applications key from manifest');
   }
+  console.log('=====================================');
 });
